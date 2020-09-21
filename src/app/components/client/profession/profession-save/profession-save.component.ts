3bdfd7e22114b377b4aa61167b5eb03086265eea
nidhro <amdadcse13@gmail.com>
2020-09-21 12:01:33 +0600

    Profession crud added

---
+++ src/app/components/client/profession/profession-save/profession-save.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ClientService} from '../../../../_services/client.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {environment} from '../../../../../environments/environment';
+
+@Component({
+  selector: 'app-profession-save',
+  templateUrl: './profession-save.component.html',
+  styleUrls: ['./profession-save.component.scss']
+})
+export class ProfessionSaveComponent implements OnInit {
+  public baseUrl = environment.apiurl.service;
+  submitted = false;
+  public pageTitle: string;
+  public brand: any;
+  public professionForm: FormGroup;
+  public professionName: any;
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private clientService: ClientService
+  ) {
+    this.professionForm = this.formBuilder.group({
+      id: '',
+      professionName: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): any {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.pageTitle = 'Edit Profession';
+      this.clientService.getProfessionById(parseInt(id)).subscribe(
+        (res: any ) => {
+          this.professionForm.patchValue({
+            id: res.id,
+            professionName: res.professionName,
+          });
+          this.brand = res;
+          console.log(this.brand);
+        }
+      );
+    } else {
+      this.pageTitle = 'Create Profession';
+    }
+  }
+
+  uploadSubmit(): void {
+    this.submitted = true;
+    if (this.professionForm.valid) {
+      const data = new FormData();
+      data.append('id', this.professionForm.controls.id.value);
+      data.append('professionName', this.professionForm.controls.professionName.value);
+      this.clientService.saveProfession(data).subscribe(
+        res => {
+          if (res === 'OK') {
+            if (this.professionForm.controls.id.value) {
+              this.router.navigate(['profession/save/' + this.professionForm.controls.id.value]);
+            } else {
+              this.router.navigate(['profession/list']);
+            }
+          }
+        });
+    }
+  }
+
+  resetAll(): void {
+    this.professionForm.controls.professionName.reset();
+  }
+}
