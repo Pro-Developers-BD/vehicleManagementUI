cc6075bef4897c3cae4bfeb554dd5a8e000c3841
atuar123 <atuar.cse@gmail.com>
2020-09-25 00:12:28 +0600

    Car grade CRUD initiated

---
+++ src/app/components/car/car-grade/car-grade-save/car-grade-save.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car-grade-save',
+  templateUrl: './car-grade-save.component.html',
+  styleUrls: ['./car-grade-save.component.scss']
+})
+export class CarGradeSaveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/car/car-grade/car-grade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car-grade',
+  templateUrl: './car-grade.component.html',
+  styleUrls: ['./car-grade.component.scss']
+})
+export class CarGradeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
